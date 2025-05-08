179cb81079415d052fc7342a3e1fcd781cc227cd
unknown <yasdmi048@gmail.com>
2025-05-07 21:39:26 -0500

    :feat :sparkles : first activity

---
+++ Python_Dia2/FindtheRunner-UpScore!_HackerRank_SanchezAlejandro.py
@@ -0,0 +1,5 @@
+
+arr = map(int, input().split())
+mayor=set(arr )
+mayor=(sorted(mayor ,reverse=True))
+print (mayor[1])
---
+++ Python_Dia2/division_hackerRank_SanchezAlejandro.py
@@ -0,0 +1,9 @@
+
+###############################
+#####       python dia2    ####
+###############################
+a = int(input())
+b = int(input())
+  
+print (float(a//b))
+print (float(a/b))
