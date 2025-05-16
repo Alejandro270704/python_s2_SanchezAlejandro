ffe2f110ff62f97f3a3786f955eb7000b8ca2b58
unknown <yasdmi048@gmail.com>
2025-05-15 21:06:42 -0500

    feat :sparkles: exercise

--- Python_Dia4/Ejercicio1_Dia4_SanchezAlejandro.py
+++ Python_Dia4/Ejercicio1_Dia4_SanchezAlejandro.py
@@ -1,4 +1,41 @@
 
 #ejercicio 1 dia 4#
+import json
+from funciones import agregarUsuario,informe
+def lista_json(Datos_json):
+    with open(Datos_json, 'r', encoding='utf-8') as archivo:
+        return json.load(archivo)
+
+lista_usuarios=lista_json("Datos.json")
+contador=True
+lista_usuarios=[]
+while contador==True:
+    
+    print ("digita la opcion ")
+    print ("1.crear artista  ")
+    print ("2. informe de reportes")
+    print ("3.modulo de reportes ")
+    print ("4.salir del programa")
+    opc=int(input())
+    if opc==1 :
+        nuevousario=agregarUsuario(1)
+        lista_usuarios.append(nuevousario)
+        print ("se ha creado el artista")
+        
+    elif opc==2:
+       reporte=informe()
+       
+    elif opc ==4 :
+        contador=False
+    else :
+        print ("esta opcion no existe ")
 
 
+       
+       
+        
+        
+        
+        
+    
+ 
--- Python_Dia4/funciones.py
+++ Python_Dia4/funciones.py
@@ -1,2 +1,21 @@
 
 #funciones 
+
+
+def agregarUsuario(opc):
+    print("Crear artista")
+    nuevoArtista= {
+        "País de origen": input("País de origen: "),
+        "Años de actividad": input("Años de actividad: "),
+        "Año de lanzamiento del primer disco que llegó a las listas": int(input("Año de lanzamiento del primer disco que llegó a las listas: ")),
+        "Género musical": input("Género musical: "),
+        "Unidades certificadas totales": input("Unidades certificadas totales: "),
+        "Ventas reclamadas": input("Ventas reclamadas: "),
+        "Estado del artista (si está activo o no)": input("¿Está activo el artista? (sí/no): ")
+    }
+    return nuevoArtista
+def informe ():
+    pais = input("Ingresa el país: ")
+    inicio = int(input("Año de inicio: "))
+    fin = int(input("Año final: "))
+    return pais, inicio, fin
